d7cd5023287fc5aaecfb250d0d78b73065bbf814
kryan39 <ryan.katie95@gmail.com>
2025-03-31 00:37:13 -0400

    build simple comps, render tiles and collections, util test

--- hulu-app/src/components/Row.ts
+++ hulu-app/src/components/Row.ts
@@ -1,22 +1,27 @@
 import type { Collection } from "../models/types";
+import { tileComponent } from "./Tile";
 
 export function rowComponent(collection: Collection, rowIndex: number): HTMLElement {
-    const row = document.createElement("div");
+    const row = document.createElement("section");
     row.className = "row";
     row.setAttribute("data-row-index", rowIndex.toString());
 
+    //builds the title for the row collection and adds it to the row section
     const title = document.createElement("h2");
     title.className = "row-title";
     title.textContent = collection.name;
     row.appendChild(title);
 
-    // const tiles = document.createElement("div");
-    // tiles.className = "tiles";
-    // collection.items.forEach((item, index) => {
-    //     const tile = tileComponent(item, index);
-    //     tiles.appendChild(tile);
-    // });
-    // row.appendChild(tiles);
+    //creates a div to hold the tiles for the collection and adds it to the row section
+    const tiles = document.createElement("div");
+    tiles.className = "tiles";
+    collection.items.forEach((item, index) => {
+        const tile = tileComponent(item, index);
+        tiles.appendChild(tile);
+     });
+    row.appendChild(tiles);
 
     return row;
 }
+
+// each collection reenders as a section with a title and a list of tiles build by tileComponent
