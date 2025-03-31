94ad0536ef03f6856e236f0791d019c9ed0aee2b
kryan39 <ryan.katie95@gmail.com>
2025-03-31 01:04:01 -0400

    added hydrating on inial fetch

--- hulu-app/src/api/fetchData.ts
+++ hulu-app/src/api/fetchData.ts
@@ -3,6 +3,23 @@ import type { Hub } from "../models/types";
 export async function fetchHubData(): Promise<Hub> {
     const response = await fetch("https://d1q0vy0v52gyjr.cloudfront.net/hub.json");
     if(!response.ok) throw new Error (`Failed to fetch hub data: ${response.statusText}`);
-    return await response.json();
+    const hubData: Hub = await response.json();
+
+    //hydrate an empty collection 
+    const hydrationPromises = hubData.components.map(async (collection) => {
+        if (collection.items.length === 0 && collection.href) {
+            try {
+                const response = await fetch(collection.href);
+                if (!response.ok) throw new Error("Failed to hydrate collection " + collection.name);
+                const hydratedCollection = await response.json();
+                collection.items = hydratedCollection.items;
+            } catch (error) {
+                console.error("Error hydrating collection", error);
+            }
+        }
+    });
+
+    await Promise.all(hydrationPromises);
+    return hubData;
 
 }
